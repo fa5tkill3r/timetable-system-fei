20a2735734511c5a209b577b4487280bba30307b
Nicolas Ondráš <nicolasko999@gmail.com>
2025-01-25 14:05:16 +0100

    chore: prettier shadcn

--- src/components/ui/data-table/DataTable.vue
+++ src/components/ui/data-table/DataTable.vue
@@ -1,40 +1,44 @@
 <script setup lang="ts" generic="TData, TValue">
-import type { ColumnDef } from '@tanstack/vue-table'
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from '@/components/ui/table'
+  import type { ColumnDef } from '@tanstack/vue-table'
+  import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from '@/components/ui/table'
 
-import {
-  FlexRender,
-  getCoreRowModel,
-  useVueTable,
-} from '@tanstack/vue-table'
+  import { FlexRender, getCoreRowModel, useVueTable } from '@tanstack/vue-table'
 
-const props = defineProps<{
-  columns: ColumnDef<TData, TValue>[]
-  data: TData[]
-}>()
+  const props = defineProps<{
+    columns: ColumnDef<TData, TValue>[]
+    data: TData[]
+  }>()
 
-const table = useVueTable({
-  get data() { return props.data },
-  get columns() { return props.columns },
-  getCoreRowModel: getCoreRowModel(),
-})
+  const table = useVueTable({
+    get data() {
+      return props.data
+    },
+    get columns() {
+      return props.columns
+    },
+    getCoreRowModel: getCoreRowModel(),
+  })
 </script>
 
 <template>
   <div class="border rounded-md">
     <Table>
       <TableHeader>
-        <TableRow v-for="headerGroup in table.getHeaderGroups()" :key="headerGroup.id">
+        <TableRow
+          v-for="headerGroup in table.getHeaderGroups()"
+          :key="headerGroup.id"
+        >
           <TableHead v-for="header in headerGroup.headers" :key="header.id">
             <FlexRender
-              v-if="!header.isPlaceholder" :render="header.column.columnDef.header"
+              v-if="!header.isPlaceholder"
+              :render="header.column.columnDef.header"
               :props="header.getContext()"
             />
           </TableHead>
@@ -43,11 +47,15 @@ const table = useVueTable({
       <TableBody>
         <template v-if="table.getRowModel().rows?.length">
           <TableRow
-            v-for="row in table.getRowModel().rows" :key="row.id"
+            v-for="row in table.getRowModel().rows"
+            :key="row.id"
             :data-state="row.getIsSelected() ? 'selected' : undefined"
           >
             <TableCell v-for="cell in row.getVisibleCells()" :key="cell.id">
-              <FlexRender :render="cell.column.columnDef.cell" :props="cell.getContext()" />
+              <FlexRender
+                :render="cell.column.columnDef.cell"
+                :props="cell.getContext()"
+              />
             </TableCell>
           </TableRow>
         </template>
