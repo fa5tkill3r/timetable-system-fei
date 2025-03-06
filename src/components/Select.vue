ee98b84ebf7835dc14351baa0a4df510da20c70b
Nicolas Ondráš <nicolasko999@gmail.com>
2025-03-06 18:19:06 +0100

    feat: select on timetable

---
+++ src/components/Select.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+  import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+  } from '@/components/ui/select'
+  import { ref, watch } from 'vue'
+
+  const props = defineProps<{
+    items: string[],
+    placeholder?: string,
+  }>()
+
+  const model = defineModel<string>()
+</script>
+
+<template>
+  <Select v-model="model">
+    <SelectTrigger class="w-[180px]">
+      <SelectValue :placeholder="props.placeholder || 'Select an option'" />
+    </SelectTrigger>
+    <SelectContent>
+      <SelectGroup>
+        <SelectItem v-for="item in props.items" :key="item" :value="item">
+          {{ item }}
+        </SelectItem>
+      </SelectGroup>
+    </SelectContent>
+  </Select>
+</template>
